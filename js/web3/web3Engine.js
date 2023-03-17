e28f39bb1642b918d9cd31bf36d40db7f046cd39
Andrea-789 <99204782+Andrea-789@users.noreply.github.com>
2023-03-17 10:21:23 +0100

    update web3engine

--- js/web3/web3Engine.js
+++ js/web3/web3Engine.js
@@ -1,10 +1,48 @@
-const serverUrl = "https://57spofk0ilpc.usemoralis.com:2053/server";
-const appId = "FqdKOiZQwsnLjQpv78qaJDBuZDrTL69332EfDsES";
-
-Moralis.start({ serverUrl, appId });
+const serverUrl = 'https://localhost:1337/api';
+const appId = YOUR_APP_ID;
 
 let userAddress = "";
 let web3 = "";
+
+const handleApiPost = async (endpoint, params) => {
+	const result = await axios.post(`${serverUrl}/${endpoint}`, params, {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+
+	return result.data;
+};
+
+const handleApiGet = async (endpoint) => {
+	console.log("get", `${serverUrl}/${endpoint}`);
+	const result = await axios.get(`${serverUrl}/${endpoint}`, {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+
+	return result.data;
+};
+
+const requestMessage = (account, networkType, chain) =>
+	handleApiPost('moralis/request-message', {
+		address: account,
+		chain: chain,
+		network: networkType,
+		DOMAIN: 'dc3.space',
+		STATEMENT: 'Please sign this message to confirm your identity.',
+		URI: 'https://dc3.space/dc3land',
+		EXPIRATION_TIME: '2024-01-01T00:00:00.000Z',
+		TIMEOUT: 30
+	});
+
+const verifyMessage = (message, signature, networkType) =>
+	handleApiPost('moralis/sign-message', {
+		message,
+		signature,
+		networkType,
+	});
 
 async function login() {
 
@@ -14,23 +52,34 @@ async function login() {
 	if (typeof ethereum === "undefined" && typeof window.ethereum === "undefined")
 		return;
 
-	//authenticate
-	try {
-		user = await Moralis.authenticate({
-			signingMessage: "Welcome to DC3Land",
-			appLogo: "https://www.dc3.digital/dc3land/img/logo/dc3ico.png",
-		});
-	} catch (err) {
-		console.log(err);
-		return;
+	web3 = new Web3(window.ethereum);
+	const provider = web3.currentProvider;
+
+	const accounts = await window.ethereum.request(
+		{ method: "eth_requestAccounts" }
+	);
+
+	let chain = "0x1";		//Eth main net
+
+	userAddress = accounts[0];
+
+	const resMessage = await requestMessage(userAddress, 'evm', chain);
+	if (resMessage.res === "KO") {
+		console.log(resMessage.msg);
+		alert("Error in sending message to Metamask");
 	}
 
-	userAddress = user.get("ethAddress");
-	console.log(userAddress);
+	const signature = await web3.eth.personal.sign(
+		web3.utils.utf8ToHex(resMessage.msg),
+		userAddress);
 
-	//switch to Mumbai Network if exists, otherwise it adds it
-	await Moralis.enableWeb3;
-	web3 = new Web3(Moralis.provider);
+	const user = await verifyMessage(resMessage.msg, signature, 'evm');
+	if (user.res === "KO") {
+		console.log(user.msg);
+		alert("Error in verifying address");
+	}
+
+	//switch to mumbai chain
 	try {
 		await web3.givenProvider.request({
 			method: "wallet_switchEthereumChain",
@@ -45,7 +94,6 @@ async function login() {
 						{
 							chainId: "0x13881",
 							chainName: "Mumbai",
-							//rpcUrls: ["https://rpc-mumbai.matic.today"],
 							rpcUrls: ["https://rpc.ankr.com/polygon_mumbai"],
 							nativeCurrency: {
 								name: "Matic",
@@ -65,27 +113,25 @@ async function login() {
 	//check if the user has already claimed a land
 	if (userAddress != "") {
 		const data = await checkUserExists();
-		console.log(data);
 		if (!data)
 			//add token symbol to metamask
 			await addTokenSymbol();
 		else
 			loadUserData(data);
-
 	}
 
 }
 
-function checkWallet() {	
+function checkWallet() {
 	if (userAddress === "") {
-		document.getElementById("conn").style.visibility = "visible"; 
-		if (typeof ethereum === "undefined" && typeof window.ethereum === "undefined"){
+		document.getElementById("conn").style.visibility = "visible";
+		if (typeof ethereum === "undefined" && typeof window.ethereum === "undefined") {
 			document.getElementById("connection").innerHTML = "Install Metamask";
 			document.getElementById("conn").style.width = "18%";
-		}		
+		}
 	}
 	else
-		document.getElementById("conn").style.visibility = "hidden"; 
+		document.getElementById("conn").style.visibility = "hidden";
 }
 
 //add the token symbol to metamask
@@ -100,7 +146,7 @@ async function addTokenSymbol() {
 					address: CONTRACT_TOKEN_ADDRESS, // The address that the token is at.
 					symbol: "DC3", // A ticker symbol or shorthand, up to 5 chars.
 					decimals: 18, // The number of decimals in the token
-					image: "https://www.dc3.digital/dc3land/img/logo/dc3ico.png" // A string url of the token logo
+					image: "https://www.dc3.space/dc3land/img/logo/dc3ico.png" // A string url of the token logo
 				}
 			},
 		});
@@ -121,7 +167,7 @@ async function addNFTSymbol() {
 					address: CONTRACT_NFT_ADDRESS, // The address that the token is at.
 					symbol: "DC3NFT", // A ticker symbol or shorthand, up to 5 chars.
 					decimals: 0, // The number of decimals in the token
-					image: "https://www.dc3.digital/dc3land/img/logo/dc3ico.png"
+					image: "https://www.dc3.space/dc3land/img/logo/dc3ico.png"
 				},
 			},
 		});
@@ -131,7 +177,7 @@ async function addNFTSymbol() {
 	}
 }
 
-//call Moralis Cloud Function to send tokens
+//api call to transfer tokens
 async function transferToken(num) {
 
 	if (userAddress === "")
@@ -139,14 +185,14 @@ async function transferToken(num) {
 
 	const params = {
 		address: userAddress,
-		amount: Moralis.Units.Token(num, "18")
+		amount: web3.utils.toWei(num.toString(), 'ether')
 	}
 
-	const res = await Moralis.Cloud.run("transferToken", params);
+	const res = await handleApiPost('moralis/transfer-token', params);
 	return res;
 }
 
-//call Moralis Cloud Function to send matic
+//api call to send matic
 async function transferMatic() {
 
 	if (userAddress === "")
@@ -154,48 +200,76 @@ async function transferMatic() {
 
 	const params = {
 		address: userAddress,
-		amount: Moralis.Units.ETH("0.002", "18")
+		amount: web3.utils.toWei('0.002', 'ether')
 	}
 
-	const res = await Moralis.Cloud.run("transferMatic", params);
+	const res = await handleApiPost('moralis/transfer-matic', params);
 	return res;
 }
 
 //check if the plot is already assigned
 async function isPlotAssigned(plotID) {
-	const contractOptions = {
-		contractAddress: CONTRACT_NFT_ADDRESS,
-		abi: CONTRACT_NFT_ABI,
-		functionName: "exist",
-		params: {
-			bytesId: plotID
-		}
-	}
-	return await Moralis.executeFunction(contractOptions);
+
+	const data = await handleApiGet(`moralis/plot-assigned/${plotID.toString()}`);
+
+	if (data.res == "OK")
+		return data.msg;
+	else
+		return null;
+
 }
 
 //save plot image to IPFS
-async function savePlotToIPFS(plotID) {
+async function savePlotImageToIPFS(plotID) {
 
 	let image = canvasLand.toDataURL("image/png");
-	const file = new Moralis.File(plotID + ".png", { base64: image });
 
-	await file.saveIPFS();
+	const params = {
+		path: plotID + ".png",
+		content: image
+	}
 
-	land.imageHash = file.hash();
-	land.imageIpfs = file.ipfs();
-	console.log(file.hash(), file.ipfs());
+	const res = await handleApiPost('moralis/save-plot', params);
 
+	if (res.res === "OK") {
+		land.imageHash = res.msg.hash;
+		land.imageIpfs = res.msg.ipfs;
+		console.log(res.msg.hash, res.msg.ipfs);
+	}
+}
+
+//save plot data to IPFS
+async function savePlotDataToIPFS(metadata) {
+
+	const params = {
+		path: "metadata.json",
+		content: btoa(JSON.stringify(metadata))
+	}
+
+	const res = await handleApiPost('moralis/save-plot', params);
+
+	if (res.res == "OK")
+		return res.msg;
+	else
+		return null;
 }
 
 //assign selected plot if it's not assigned
 async function assignPlot() {
 	const plot = { tileX: land.tile.x, tileY: land.tile.y, positionX: land.absPosition.x, positionY: land.absPosition.y };
-	const plotID = Moralis.web3Library.utils.id(JSON.stringify(plot));
+	const plotID = web3.utils.sha3(JSON.stringify(plot));
 	const assigned = await isPlotAssigned(plotID);
+
+	if (assigned == null) {
+		return {
+			result: "KO",
+			msg: "Error in Plot assignment"
+		};
+	}
+
 	if (!assigned) {
 
-		await savePlotToIPFS(plotID);
+		await savePlotImageToIPFS(plotID);
 
 		const metadata = {
 			"PlotID": plotID,
@@ -207,30 +281,44 @@ async function assignPlot() {
 			"imageIpfs": land.imageIpfs
 		}
 
-		const metadataFile = new Moralis.File("metadata.json", { base64: btoa(JSON.stringify(metadata)) });
-		await metadataFile.saveIPFS();
-		const metadataURI = metadataFile.ipfs();
+		const metadataFile = await savePlotDataToIPFS(metadata);
+		const metadataURI = metadataFile.ipfs;
 		land.ipfs = metadataURI;
+
 		const res = await mint(metadataURI, plotID);
-		console.log("after mint res", res);
-		// Save reference to Moralis
-		if (res.result === "OK") {
-			try {
-				const plot = new Moralis.Object("Plots");
-				plot.set("address", userAddress);
-				plot.set("metadata", metadata);
-				plot.set("ipfs", metadataURI);
-				await plot.save();
-			} catch (err) {
-				console.log("IPFS error", err);
+
+		try {
+			const params = {
+				address: userAddress,
+				metadata: JSON.stringify(metadata),
+				ipfs: metadataURI
+			}
+			
+			const data = await handleApiPost('plot', params);
+
+			if (data.res == "OK") {
+				claimed = true;
+			} else {
+				return {
+					result: "KO",
+					msg: "Save data error"
+				};
 			}
 		}
-		console.log(res);
-		claimed = true;
-		return res;
+		catch (error) {
+			console.log("Save data error", error);
+			return {
+				result: "KO",
+				msg: "Save data error"
+			};
+		}		
+
+		return {
+			result: "OK",
+			msg: res.msg
+		}		
 	}
 	else {
-		console.log("Plot is already assigned");
 		return {
 			result: "KO",
 			msg: "Plot is already assigned"
@@ -239,25 +327,25 @@ async function assignPlot() {
 }
 
 async function mint(tokenURI, plotID) {
-	const contractOptions = {
-		contractAddress: CONTRACT_NFT_ADDRESS,
-		abi: CONTRACT_NFT_ABI,
-		functionName: "assign",
-		params: {
-			tokenURI: tokenURI,
-			bytesId: plotID
-		}
-	}
+
 	try {
-		const transaction = await Moralis.executeFunction(contractOptions);
-		await transaction.wait();
-		console.log("Transaction confirmed with hash " + transaction.hash);
-		console.log(transaction);
-		return {
-			result: "OK",
-			msg: transaction.hash
-		};
-	}
+		const contract = new web3.eth.Contract(CONTRACT_NFT_ABI, CONTRACT_NFT_ADDRESS);
+
+		const hashData = await contract.methods.assign(tokenURI, plotID)
+			.send({ from: userAddress });
+
+		if (hashData) {
+			return {
+				result: "OK",
+				msg: hashData.events.Transfer.transactionHash
+			}
+		}	else {
+			return {
+				result: "KO",
+				msg: "Transaction reverted"
+			};
+		}
+	} 
 	catch (error) {
 		console.log("Transaction reverted", error);
 		return {
@@ -267,17 +355,21 @@ async function mint(tokenURI, plotID) {
 	}
 }
 
-//check if user exists on Moralis
+//check if user exists
 async function checkUserExists() {
-	const query = new Moralis.Query("Plots");
-	query.equalTo("address", userAddress);
-	const data = await query.find();	//	.then(function ([plot]) {
-	
-	if (data.length > 0)
-		return {
-			data: data[0].get("metadata"),
-			ipfs: data[0].get("ipfs")
-		}
-	else
+
+	try {
+		const data = await handleApiGet(`plot/${userAddress.toString()}`);
+
+		if (data.res == 'OK')
+			return {
+				metadata: data.msg.metadata,
+				ipfs: data.msg.ipfs
+			}
+		else
+			return null;
+	} catch (e) {
+		console.log("error", e);
 		return null;
+	}
 }
